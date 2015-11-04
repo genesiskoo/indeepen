94fd8a70cac9c234c9dc3266f0113e082683a4d1
MooN <answjdgus222@naver.com>
2015-11-04 13:14:11 +0900

    1104 1: 13 날리고 난 직후

--- models/Users.js
+++ models/Users.js
@@ -1,21 +1,18 @@
-var Counters = require('./Counters.js');
+
 var User = require('./schemas/Users.js');
 
 module.exports.saveUser = function (userInfo, callback) {
-	Counters.getNextSeq('users', function (err, seq) {
-		if (err) {
-			console.error("ERROR OF GETTING AUTO_INCREMENT :", err);
-			return;
-		}
-		console.log('next sq', seq);
-		userInfo._id = seq.seq;
-		User.create(userInfo, callback);
-	});
+    User.create(userInfo, callback);
 }
 
 module.exports.findOne = function(key, callback){
-	User.findOne(key, '_id email nick profile_photo is_valid.is_valid',callback);
+    User.findOne(key, '_id email nick profile_photo is_valid.is_valid',callback);
 }
+
+
+
+
+
 
 
 

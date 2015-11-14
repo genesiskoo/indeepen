4da7b5d09e939aec7f84479fbccc6bc416f2cf3e
MooN <answjdgus222@naver.com>
2015-11-14 17:22:27 +0900

    add controllers/routers - Users

---
+++ controllers/Users.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Moon Jung Hyun on 2015-11-14.
+ */
+
+var User = require('./../models/Users');
+
+module.exports.join = function(req, res, next){
+
+};
+
+module.exports.checkEmail = function(req, res, next){
+
+};
+
+module.exports.changePw = function(req, res, next){
+
+};
+
+module.exports.getUserInfo = function(req, res, next){
+
+};
+
+module.exports.changeActivityMode = function(req, res, next){
+
+};

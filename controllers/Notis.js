542c734c7cf2cfd84633f5b904fe3628e8959a21
genesiskoo <genesiskoo@gmail.com>
2015-11-24 11:46:44 +0900

    Notis 진행중

--- controllers/Notis.js
+++ controllers/Notis.js
@@ -2,16 +2,55 @@
  * Created by skplanet on 2015-11-24.
  */
 
-//var Noti = require('./../models/Notis');
+var Noti = require('./../models/Notis');
 
-module.exports.getNotis = function (req, res, next){
-    res.end('getNotis');
+var sender = '564aa1d94287c23c068b95dc'; //결석왕Blog
+
+module.exports.getNotis = function (req, res, next) {
+    Noti.findAll(function (err, docs) {
+        res.json(docs);
+    });
 };
 
-module.exports.makeNoti = function (req, res, next){
-    res.end('makeNotis');
+module.exports.makeNoti = function (req, res, next) {
+    var type = req.body.type;
+
+    var notiInfo = {
+        _sender: sender,
+        _receiver: req.body.receiver,
+        where : {
+            type : type
+        },
+        how: req.body.how
+    };
+    switch (type) {
+        case '1':
+        case '2':
+            notiInfo.where['_postId'] = '5644187a4ead9a2416e77470'; //슈퍼스타post
+            break;
+        case '3':
+        case '4':
+            notiInfo.where['_blogId'] = '564aa1d94287c23c068b95dc'; //문개인블로그
+            break;
+        case '5':
+            //협력제안 준비중
+            break;
+        default:
+        //type에러처리
+    }
+    Noti.saveNoti(notiInfo, function (err) {
+        if (err) {
+            console.error('err :',err);
+            return;
+        }
+        var msg = {
+            code: 200,
+            msg: '알림 입력 성공'
+        };
+        res.status(msg.code).json(msg);
+    });//saveNoti
 };
 
-module.exports.checkNoti = function (req, res, next){
+module.exports.checkNoti = function (req, res, next) {
     res.end('checkNotis');
 };

57a968d8b399eeab4dafd4eacbbbadcaa93ceab1
MooN <answjdgus222@naver.com>
2015-11-25 19:09:49 +0900

    login 수정3

--- controllers/TheRest.js
+++ controllers/TheRest.js
@@ -2,28 +2,6 @@
  * Created by Moon Jung Hyun on 2015-11-17.
  */
 
-
-
-module.exports.loginSuc = function(req, res){
-    var user = req.user;
-    console.log('loginSuc ', user);
-    var GCMKey = req.body.GCMKey;     // 이거 db에 추가해 줘야 할걸...???
-    console.log('GCMKey ', GCMKey);
-    var msg = {
-        code : 200,
-        msg : 'Success',
-        result : user
-    };
-    console.log('msg ', msg);
-    res.status(msg.code).json(msg);
-};
-
-module.exports.logFail = function(req, res, next){
-    var error = new Error('로그인 실패..');
-    error.code = 401;
-    return next(error);
-};
-
 module.exports.logout = function(req, res){
     req.logout();   // passport session 지우기
     // session 지워줌...
--- routers/TheRest.js
+++ routers/TheRest.js
@@ -6,41 +6,34 @@ var TheRest = require('./../controllers/TheRest');
 //var auth = require('./../config/middlewares/authorization');
 
 module.exports = function(app, passport){
-    /*app.post('/login',
-        passport.authenticate('local',{
-            successRedirect : '/loginSuc',
-            failureRedirect : '/loginFail'
-        }));*/
     app.post('/login',
-       /* passport.authenticate('local',{
-            successRedirect : '/loginSuc',
-            failureRedirect : '/loginFail'
-        })*/
         function(req, res) {
             passport.authenticate('local', function(err, user, msg, statusCode) {
                 if ( ! user ) {
-                    res.status(401).json(msg);
-                    return;
-                }
-                // 세션에 기록
-                req.logIn(user, function(err) {
-                    if ( err ) {
-                        res.status(401).json({msg:'Session Write Error'});
-                        return;
-                    }
                     var msg = {
-                        code : 200,
-                        msg : 'Success',
-                        result : user
+                        code : 401,
+                        msg : msg.message
                     };
-                    console.log('msg ', msg);
                     res.status(msg.code).json(msg);
-                });
-            })(req);}
-    );
+                }else{
+                    // 세션에 기록
+                    req.logIn(user, function(err) {
+                        if ( err ) {
+                            res.status(401).json({code : 401, msg:'Session Write Error'});
+                            return;
+                        }
+                        var msg = {
+                            code : 200,
+                            msg : 'Success',
+                            result : user
+                        };
+                        console.log('msg ', msg);
+                        res.status(msg.code).json(msg);
+                    });
+                }
+            })(req);
+        });
 
-    app.get('/loginSuc', TheRest.loginSuc);
-    app.get('/loginFail', TheRest.logFail);
 
     app.get('/logout', TheRest.logout);
 

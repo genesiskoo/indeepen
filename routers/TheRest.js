3492bb39e28ba2b76af67b84d9b50e6b4f44284c
MooN <answjdgus222@naver.com>
2015-11-25 18:25:27 +0900

    login 수정

--- routers/TheRest.js
+++ routers/TheRest.js
@@ -6,11 +6,37 @@ var TheRest = require('./../controllers/TheRest');
 //var auth = require('./../config/middlewares/authorization');
 
 module.exports = function(app, passport){
-    app.post('/login',
+    /*app.post('/login',
         passport.authenticate('local',{
             successRedirect : '/loginSuc',
             failureRedirect : '/loginFail'
-        }));
+        }));*/
+    app.post('/login',
+       /* passport.authenticate('local',{
+            successRedirect : '/loginSuc',
+            failureRedirect : '/loginFail'
+        })*/
+        function(req, res) {
+            passport.authenticate('local', function(err, user, msg, statusCode) {
+                if ( ! user ) {
+                    res.status(401).json(msg);
+                    return;
+                }
+                // 세션에 기록
+                req.logIn(user, function(err) {
+                    if ( err ) {
+                        res.status(401).json({msg:'Session Write Error'});
+                        return;
+                    }
+                    var msg = {
+                        code : 200,
+                        msg : 'Success',
+                        result : user
+                    };
+                    res.status(msg.code).json(msg);
+                });
+            })(req);}
+    );
 
     app.get('/loginSuc', TheRest.loginSuc);
     app.get('/loginFail', TheRest.logFail);
